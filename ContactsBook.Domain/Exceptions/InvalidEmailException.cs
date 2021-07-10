097d855b3718f6acfbb95fe35f282849f62fa09f
temicch <temicch@gmail.com>
2021-07-11 01:18:36 +0700

    feat: value objects own exceptions

---
+++ ContactsBook.Domain/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ContactsBook.Domain.Exceptions
+{
+    public class InvalidEmailException: ArgumentException
+    {
+        public override string ParamName { get; } = "Email";
+
+        public InvalidEmailException(): base()
+        {
+
+        }
+    }
+}
