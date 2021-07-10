cc6df176a17baa78139f0797c5aa10f179811422
temicch <temicch@gmail.com>
2021-07-10 23:15:28 +0700

    chore: settings changes
     - Change npm to yarn
     - Added vue-loader for 'one file vue template' work

--- ContactsBook.WebUI/webpack.config.js
+++ ContactsBook.WebUI/webpack.config.js
@@ -1,81 +1,82 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+var webpack = require("webpack");
 
 let config = {
-    entry: {
-        main: './ClientApp/src/main.ts'
+  entry: {
+    main: "./ClientApp/src/main.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "ClientApp/dist"),
+    publicPath: "/styles",
+    filename: "[name].js",
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+      "@": path.resolve(__dirname, "ClientApp/src"),
     },
-    output: {
-        path: path.resolve(__dirname, 'ClientApp/dist'),
-        publicPath: '/styles',
-        filename: '[name].js'
-    },
-    resolve: {
-      alias: {
-        // Ensure the right Vue build is used
-        'vue$': 'vue/dist/vue.esm.js',
-        '@': path.resolve(__dirname, 'ClientApp/src'),
+    extensions: [".ts", ".js", ".json", ".vue"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: { appendTsSuffixTo: [/\.vue$/] },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+
+        // Use Babel to transpile ES6+ to ES5
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        extensions: ['.ts', '.js', '.json']
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                // Look for JavaScript files and process them according to the
-                // rules specified in the different loaders
-                test: /\.(js)$/,
+      },
+      {
+        test: /\.scss$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
 
-                // Ignore the node_modules directory
-                exclude: /node_modules/,
-
-                // Use Babel to transpile ES6+ to ES5
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-              test: /\.s[ac]ss$/i,
-              use: [
-                // Creates `style` nodes from JS strings
-                "style-loader",
-                // Translates CSS into CommonJS
-                "css-loader",
-                // Compiles Sass to CSS
-                "sass-loader",
-              ],
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.svg/,
-                type: 'asset/inline'
-            },
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "vue-style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg/,
+        use: ["vue-loader", "vue-svg-loader"],
+      },
     ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      "process.env.BUILD": JSON.stringify("web"),
+    }),
+  ],
 };
 
 module.exports = (env, argv) => {
-    if(argv.mode == 'development') {
-        config.devtool = 'inline-source-map';
-        config.optimization.minimize = false;
-    } 
-    return config;
+  if (argv.mode == "development") {
+    config.devtool = "inline-source-map";
+    config.optimization.minimize = false;
+  }
+  return config;
 };
